8a696cc8a6c2e107c882ecc751374f0af9cbebdd
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-11 22:02:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-// Complete the js code
-function Car(make, model) {
-	this.make=make;
-	this.model=model;
-}
-Car.prototype.getMakeModel=function() {
-	return `${this.make} ${this.model}`;
-	
+class Car{
+    constructor(make,model){
+        this.make=make;
+        this.model=model;
+
+    }
+    getMakeModel(){
+        return `${this.make} ${this.model}`;
+    }
 }
 
-function SportsCar(make, model, topSpeed) {
-	Car.call(this,make,model);
-	this.topSpeed=topSpeed;
-}
-SportsCar.prototype=Object.created(Car.prototype);
-SportsCar.prototype.constructor=SportsCar;
-SportsCar.prototype.getTopSpeed=function () {
-	return this.topSpeed;
-	
+class SportsCar extends Car{
+    constructor(make, model, topSpeed){
+        super(make,model);
+        this.topSpeed=topSpeed;
+    }
+    getTopSpeed()
+    {
+        return this.topSpeed;
+    }
 }
 const car = new SportsCar("Ferrari", "Testarossa", 200);
 console.log(car.getMakeModel()); // Output: Ferrari Testarossa
